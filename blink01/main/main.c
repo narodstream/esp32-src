ba16e41bf395bb13833a163eeec4c99e8b86c937
Vladimir <vlad_vorobyev@mail.ru>
2019-12-07 15:01:36 +0300

    add project blink01

---
+++ blink01/main/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+//-----------------------------------------------
+#define BLINK_GPIO GPIO_NUM_2
+//-----------------------------------------------
+void app_main(void)
+{
+ gpio_pad_select_gpio(BLINK_GPIO);
+gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+ while (1)
+ {
+   gpio_set_level(BLINK_GPIO, 0);
+   vTaskDelay(500 / portTICK_PERIOD_MS);
+   gpio_set_level(BLINK_GPIO, 1);
+   vTaskDelay(500 / portTICK_PERIOD_MS);
+ }
+}
+//-----------------------------------------------
+
